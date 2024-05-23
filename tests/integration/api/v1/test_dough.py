658ca76ebadd1910a9d3dae73aa39574f54ad205
Ante Brähler <ante.braehler@stud.h-da.de>
2024-05-23 16:37:34 +0200

    test: fix flake8 spacing issues and restructure tests

--- tests/integration/api/v1/test_dough.py
+++ tests/integration/api/v1/test_dough.py
@@ -1,11 +1,10 @@
+from decimal import Decimal
 import uuid
-
 import pytest
 
 import app.api.v1.endpoints.dough.crud as dough_crud
 from app.api.v1.endpoints.dough.schemas import DoughCreateSchema
 from app.database.connection import SessionLocal
-from tests.integration.api.v1.helper import clear_db
 
 
 @pytest.fixture(scope='module')
@@ -18,61 +17,79 @@ def db():
 
 
 def test_dough_create_read_update_delete(db):
-    clear_db(db)
+    new_dough_name = 'test'
+    new_dough_description = 'test description'
+    new_dough_stock = 1
+    changed_dough_stock = 3
+    new_dough_price = Decimal('1.23')
+    number_of_doughs_before = len(dough_crud.get_all_doughs(db))
 
-    # Instantiate a dough
-    dough = DoughCreateSchema(name='test dough', stock=1, price=1, description='test description')
+    # Arrange: Instantiate a dough
+    dough = DoughCreateSchema(
+        name=new_dough_name,
+        stock=new_dough_stock,
+        price=new_dough_price,
+        description=new_dough_description,
+    )
 
-    # Add dough to database
-    dough_crud.create_dough(dough, db)
+    # Act: Add dough to database
+    dough = dough_crud.create_dough(dough, db)
+    created_dough_id = dough.id
 
-    # Proof correct number of doughs in database
+    # Assert: One more dough in database
     doughs = dough_crud.get_all_doughs(db)
-    assert len(doughs) == 1
+    assert len(doughs) == number_of_doughs_before + 1
 
-    # Proof correct values of doughs in database
-    dough = doughs[0]
-    assert dough.name == 'test dough'
-    assert dough.stock == 1
-    assert dough.price == 1
-    assert dough.description == 'test description'
+    # Act: Re-read dough from database
+    dough = dough_crud.get_dough_by_id(created_dough_id, db)
 
-    doughs = dough_crud.get_all_doughs(db)
-    assert len(doughs) == 1
+    # Assert: Proof correct values of dough in database
+    assert dough.name == new_dough_name
+    assert dough.stock == new_dough_stock
+    assert dough.price == new_dough_price
+    assert dough.description == new_dough_description
 
-    # Try to get dough with invalid id
-    dough = dough_crud.get_dough_by_id(uuid.UUID('00000000-0000-0000-0000-000000000000'), db)
-    assert dough is None
+    # Assert: Try to get dough with invalid id
+    invalid_dough = dough_crud.get_dough_by_id(uuid.UUID('00000000-0000-0000-0000-000000000000'), db)
+    assert invalid_dough is None
 
-    dou_id = doughs[0].id
-    dough = dough_crud.get_dough_by_id(dou_id, db)
-    assert dough == doughs[0]
+    # Assert: Try to get dough with invalid name
+    invalid_dough = dough_crud.get_dough_by_name('invalid name', db)
+    assert invalid_dough is None
 
-    # Try to get dough with invalid name
-    dough = dough_crud.get_dough_by_name('invalid name', db)
-    assert dough is None
+    # Act: Get correct dough by name
+    dough = dough_crud.get_dough_by_name(new_dough_name, db)
 
-    dou_name = doughs[0].name
-    dough = dough_crud.get_dough_by_name(dou_name, db)
-    assert dough == doughs[0]
+    # Assert: Proof correct values of dough in database
+    assert dough.name == new_dough_name
+    assert dough.stock == new_dough_stock
+    assert dough.price == new_dough_price
+    assert dough.description == new_dough_description
 
-    changed_dough = DoughCreateSchema(name='test dough1', stock=11, price=11, description='test description1')
-    dough_crud.update_dough(doughs[0], changed_dough, db)
+    # Act: Change dough stock
+    changed_stock_dough = DoughCreateSchema(
+        name=new_dough_name,
+        stock=changed_dough_stock,
+        price=new_dough_price,
+        description=new_dough_description,
+    )
+    dough_crud.update_dough(dough, changed_stock_dough, db)
+    dough = dough_crud.get_dough_by_id(created_dough_id, db)
 
-    dough = doughs[0]
-    assert dough.name == 'test dough1'
-    assert dough.stock == 11
-    assert dough.price == 11
-    assert dough.description == 'test description1'
+    # Assert: Dough stock updated
+    assert dough.stock == changed_dough_stock
 
-    # try to delete dough with invalid id
+    # Assert: try to delete dough with invalid id
     dough_crud.delete_dough_by_id(uuid.UUID('00000000-0000-0000-0000-000000000000'), db)
-    assert len(doughs) == 1
+    assert len(doughs) == number_of_doughs_before + 1
 
-    # Delete dough
-    dough_crud.delete_dough_by_id(doughs[0].id, db)
-    # Proof correct number of doughs in database after deletion
+    # Act: Delete dough
+    dough_crud.delete_dough_by_id(created_dough_id, db)
+
+    # Assert: Proof correct number of doughs in database after deletion
     doughs = dough_crud.get_all_doughs(db)
-    assert len(doughs) == 0
+    assert len(doughs) == number_of_doughs_before
 
-    clear_db(db)
+    # Assert: Correct user was deleted
+    dough = dough_crud.get_dough_by_id(created_dough_id, db)
+    assert dough is None
